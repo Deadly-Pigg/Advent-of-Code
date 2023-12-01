190d6bee658e51835e7d96fc49ef9f596adb8058
Lex <100409468+Deadly-Pigg@users.noreply.github.com>
2023-12-01 22:19:21 +0000

    a

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
 //Credit goes to @mohfez on github for the idea
 
 //Day 1
-Console.WriteLine($"Day: 1:   \n- part 1: {new Day1().Answer(1)}, part 2: {new Day1().Answer(2)}\n\n");
+Console.WriteLine($"Day: 1:   \n- part 1: {new Day1().Answer(false)}, part 2: {new Day1().Answer(true)}\n\n");
 
 //Day 2
 //Console.WriteLine($"Day: 2:   \n- part 1: {new Day2().Answer(1)}, part 2: {new Day2().Answer(2)}\n\n");
