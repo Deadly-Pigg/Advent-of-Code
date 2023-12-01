ff7fcd7923353576bdf16ab5174f7dab67a377ef
Lex <100409468+Deadly-Pigg@users.noreply.github.com>
2023-12-01 22:25:18 +0000

    Made formatting nicer

--- Day 1/Day1.cs
+++ Day 1/Day1.cs
@@ -4,17 +4,18 @@ public class Day1
 {
     private string[] replaceValues = { "nine", "eight", "seven", "six", "five", "four", "three", "two", "one" };   
     private string[] toReplaceTo = { "9", "8", "7", "6", "5", "4", "3", "2", "1" };
+    
     public int Answer(bool part2) {
         string[] file = File.ReadAllLines("AoC1.txt"); //reading from file in the bin
-        
         int sum = 0;
 
         foreach (string line in file) { //loops through each line in the input
-        string newline = line;
-        if(part2)
-            newline = ReplaceFirst(line); //part 2, comment out for part 1.
-        newline = Regex.Replace(newline, @"\D", ""); //gets rid of all non-integer characters
-        sum += 10 * (newline[0] - '0') + (newline[newline.Length - 1] - '0'); //takes first and last number, and adds them to the sum.
+            string newline = line;
+            if(part2)
+                newline = ReplaceFirst(line); //part 2, comment out for part 1.
+            
+            newline = Regex.Replace(newline, @"\D", ""); //gets rid of all non-integer characters
+            sum += 10 * (newline[0] - '0') + (newline[newline.Length - 1] - '0'); //takes first and last number, and adds them to the sum.
         }
         return sum;
     }
@@ -39,24 +40,20 @@ public class Day1
         }
         
         for (int i = 0; i <= boundary; i++) { //incase there's any strings that are word numbers, we convert the first and last one, respectively
-            //first string number
             if (i != first.Length) //once a number has been converted, it ends the loop since there's no need to do any further operations
                 break;
-
+            
             first.Append(newline[i]); //appending to the stringbuilder the first character
-
             for (int j = 0; j < 9; j++) { //replaces any words with their numbers once.
                 first = first.Replace(replaceValues[j], toReplaceTo[j]);
             }
         }
 
-        for (int i = length-1; i >= lastBoundary; i--) { //last string number. all operations are the same, but in reverse.
-
+        for (int i = length-1; i >= lastBoundary; i--) { //all operations are the same, but in reverse.
             if (length-i-1 != last.Length)
                 break;
-
+            
             last.Insert(0,newline[i]);
-
             for (int j = 0; j < 9; j++) {
                 last = last.Replace(replaceValues[j], toReplaceTo[j]);
             }
