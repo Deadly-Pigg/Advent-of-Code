704271ae3d293af1c217cfb09be679343b8abbc4
Lex <100409468+Deadly-Pigg@users.noreply.github.com>
2023-12-01 22:14:37 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿//This is a running log of all the days and their answers. It will output each day (that I have completed) and their seperate parts individually
+//if you wish to test each day individually, comment out other days lines
+//and finally, if you want to test it against your input (cheater), then replace the text in the individual bins of each folder. Hope you enjoy
+
+//Credit goes to @mohfez on github for the idea
+
+//Day 1
+Console.WriteLine($"Day: 1:   \n- part 1: {new Day1().Answer(1)}, part 2: {new Day1().Answer(2)}\n\n");
+
+//Day 2
+Console.WriteLine($"Day: 2:   \n- part 1: {new Day2().Answer(1)}, part 2: {new Day2().Answer(2)}\n\n");
