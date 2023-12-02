ebc5d3c04f9b2a246fc67ff8c06dbbf22ba10ca7
Lex <100409468+Deadly-Pigg@users.noreply.github.com>
2023-12-02 16:55:50 +0000

    Added day 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿//This is a running log of all the days and their answers. It will output each day (that I have completed) and their seperate parts individually
+//This is a running log of all the days and their answers. It will output each day (that I have completed) and their seperate parts individually
 //if you wish to test each day individually, comment out other days lines
-//and finally, if you want to test it against your input (cheater), then replace the text in the individual bins of each folder. Hope you enjoy!
+//and finally, if you want to test it against your input (cheater), then replace the text in the individual bins of each folder. Hope you enjoy
 
 //Credit goes to @mohfez on github for the idea
 
@@ -8,4 +8,4 @@
 Console.WriteLine($"Day: 1:   \n- part 1: {new Day1().Answer(false)}, part 2: {new Day1().Answer(true)}\n\n");
 
 //Day 2
-//Console.WriteLine($"Day: 2:   \n- part 1: {new Day2().Answer(1)}, part 2: {new Day2().Answer(2)}\n\n");
+Console.WriteLine($"Day: 2:   \n- part 1: {new Day2().Answer(false)}, part 2: {new Day2().Answer(true)}\n\n");
