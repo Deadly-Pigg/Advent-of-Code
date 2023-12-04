bbbe3c5f94f67c78f30efdd35c440e5f297044d2
Lex <100409468+Deadly-Pigg@users.noreply.github.com>
2023-12-04 16:07:12 +0000

    Added day 4

--- Program.cs
+++ Program.cs
@@ -5,10 +5,14 @@
 //Credit goes to @mohfez on github for the idea
 
 //Day 1
-Console.WriteLine($"Day: 1:   \n- part 1: {new Day1().Answer(false)}, part 2: {new Day1().Answer(true)}\n\n");
+//Console.WriteLine($"Day: 1:   \n- part 1: {new Day1().Answer(false)}, part 2: {new Day1().Answer(true)}\n\n");
 
 //Day 2
-Console.WriteLine($"Day: 2:   \n- part 1: {new Day2().Answer(false)}, part 2: {new Day2().Answer(true)}\n\n");
+//Console.WriteLine($"Day: 2:   \n- part 1: {new Day2().Answer(false)}, part 2: {new Day2().Answer(true)}\n\n");
 
 //Day 3
-Console.WriteLine($"Day: 3:   \n- part 1: {new Day3().Answer(false)}, part 2: {new Day3().Answer(true)}\n\n");
+//Console.WriteLine($"Day: 3:   \n- part 1: {new Day3().Answer(false)}, part 2: {new Day3().Answer(true)}\n\n");
+
+//Day 4
+Console.WriteLine($"Day: 4:   \n- part 1: {new Day4().Answer(false)}, part 2: {new Day4().Answer(true)}\n\n");
+
