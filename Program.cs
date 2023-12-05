93c50d3bdb181a1c39da66bdc55de0a3ac534fb1
Lex <100409468+Deadly-Pigg@users.noreply.github.com>
2023-12-05 10:19:31 +0000

    Added comments

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 //This is a running log of all the days and their answers. It will output each day (that I have completed) and their seperate parts individually
 //if you wish to test each day individually, comment out other days lines
-//and finally, if you want to test it against your input (cheater), then replace the text in the individual bins of each folder. Hope you enjoy
+//if you are stuck, I'd suggesting using the testcases that are provided by the website (or, if you are really stuck, compare your code with mine, or just try and understand the approach I used). 
+//I purged my puzzle inputs from the github, and if you are curious why, read the README.md document.
 
 //Credit goes to @mohfez on github for the idea
 
