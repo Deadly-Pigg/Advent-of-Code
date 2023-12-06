f57519307979a7a168ff6fb60ac7e5258af9801a
Lex <100409468+Deadly-Pigg@users.noreply.github.com>
2023-12-06 18:34:05 +0000

    For the program initialising, you can ignore this.

---
+++ Day 0/Day0.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+
+public class Day0 : Days<string>
+{
+    public override string Answer(bool part2) {
+        StringBuilder initialiser = new StringBuilder(Regex.Replace(@"333333", @"\d", ""));
+        if(!part2)
+            return "setting up the benchmarking";
+        return "you can ignore this";
+    }
+}
