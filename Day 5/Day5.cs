a753498b09bd375a871b7907e9e00c85ceac7921
Lex <100409468+Deadly-Pigg@users.noreply.github.com>
2023-12-06 18:28:00 +0000

    Optimised Day 5 + changes

--- Day 5/Day5.cs
+++ Day 5/Day5.cs
@@ -1,5 +1,5 @@
-﻿using System.Text.RegularExpressions;
-public class Day5
+using System.Text.RegularExpressions;
+public class Day5 : Days<long>
 {
     private struct SeedData
     {
@@ -12,52 +12,65 @@ public class Day5
             Changes = change;
         } //I know about setters and getters, they just didn't seem to work properly so I abandoned them this time...
     }
-    public long Answer(bool part2) 
+    
+    public override long Answer(bool part2) 
     { 
         string[] file = File.ReadAllLines("AoC5.txt"); 
-        long min = Int64.MaxValue; //both parts want a minimum location
-        List<SeedData> seeds = new();
         MatchCollection match = Regex.Matches(file[0],@"(\d+)"); //less regex than usual. Just parses the seeds
 
-        if(!part2) 
-        {
-            for(int i = 0; i < match.Count; i++) //each individual seed
-                seeds.Add(new SeedData(long.Parse(match[i].Value)));
+        return part2 ? Part2(file, match) : Part1(file, match);
+    }
+    
+    private long Part1(string[] file, MatchCollection match) 
+    {
+        long min = Int64.MaxValue;
+        List<SeedData> seeds = new();
+        for(int i = 0; i < match.Count; i++) //each individual seed
+            seeds.Add(new SeedData(long.Parse(match[i].Value)));
 
-            foreach(string line in file) 
-            {
-                MatchCollection newLine = Regex.Matches(line, @"(\d+)");
-                if(newLine.Count != 3) //when the count == 0, it basically just means that the seeds will be moving to their next location
-                { //so, that means all of them get a fresh new 'False' to the change variable!
-                    ClearSeeds(seeds);
-                    continue;
-                }
-                ChangeVal(seeds, long.Parse(newLine[0].Value), long.Parse(newLine[1].Value), long.Parse(newLine[2].Value));
-            }
-        }
-        else
+        foreach(string line in file) 
         {
-            for(int i = 0; i < match.Count; i+=2) //each range of seed (from initial seed to the last seed)
-                seeds.Add(new SeedData(long.Parse(match[i].Value), 
-                                       long.Parse(match[i].Value) + 
-                                       long.Parse(match[i+1].Value) - 1));
-
-            foreach(string line in file)
-            {
-                MatchCollection newLine = Regex.Matches(line, @"(\d+)");
-                if(newLine.Count != 3) 
-                {
-                    ClearSeeds(seeds);
-                    continue;
-                }
-                Part2Changes(seeds, long.Parse(newLine[0].Value), long.Parse(newLine[1].Value), long.Parse(newLine[2].Value));
+            MatchCollection newLine = Regex.Matches(line, @"(\d+)");
+            if(newLine.Count != 3) //when the count == 0, it basically just means that the seeds will be moving to their next location
+            { //so, that means all of them get a fresh new 'False' to the change variable!
+                ClearSeeds(seeds);
+                continue;
             }
+            ChangeVal(seeds, long.Parse(newLine[0].Value), long.Parse(newLine[1].Value), long.Parse(newLine[2].Value));
         }
-        foreach(SeedData i in seeds) //finding minimum.
+
+        foreach(SeedData i in seeds)
             min = (long)Math.Min(i.Value, min);
 
         return min;
     }
+    
+    private long Part2(string[] file, MatchCollection match) 
+    {
+        long min = Int64.MaxValue;
+        List<SeedData> seeds = new();
+        for(int i = 0; i < match.Count; i+=2) //each range of seed (from initial seed to the last seed)
+            seeds.Add(new SeedData( long.Parse(match[i].Value), 
+                                    long.Parse(match[i].Value) + 
+                                    long.Parse(match[i+1].Value) - 1));
+
+        foreach(string line in file)
+        {
+            MatchCollection newLine = Regex.Matches(line, @"(\d+)");
+            if(newLine.Count != 3) 
+            {
+                ClearSeeds(seeds);
+                continue;
+            }
+            Part2Changes(seeds, long.Parse(newLine[0].Value), long.Parse(newLine[1].Value), long.Parse(newLine[2].Value));
+        }
+
+        foreach(SeedData i in seeds)
+            min = (long)Math.Min(i.Value, min);
+
+        return min;
+    }
+    
     private void ChangeVal(List<SeedData> seeds, long dest, long source, long length) 
     {
         for(int i = 0; i < seeds.Count; i++) //simply checks if the seed is within the source provided by the file
@@ -68,6 +81,7 @@ public class Day5
             } //yes, I used a constructor. it just changes the position of the seed to the destination
         }
     }
+    
     private void Part2Changes(List<SeedData> seeds, long dest, long source, long length) 
     {
         int len = seeds.Count;
@@ -97,7 +111,9 @@ public class Day5
             }
         }
     }
-    private void ClearSeeds(List<SeedData> seeds) { 
+    
+    private void ClearSeeds(List<SeedData> seeds) 
+    { 
         for(int i = 0; i < seeds.Count; i++)
             seeds[i] = new SeedData(seeds[i].Value, seeds[i].Range);
     } //literally just so the code looks neater. Just changes all seeds.Changes to false
